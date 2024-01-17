33a6d4de6564f65e262e2b55e218ed3a7cbb0373
Kamal492 <58765013+Kamal492@users.noreply.github.com>
2024-01-17 22:42:11 +0530

    Update singlePost.jsx

--- src/components/singlePost/singlePost.jsx
+++ src/components/singlePost/singlePost.jsx
@@ -60,7 +60,7 @@ export default function SinglePost() {
     textDecoration: 'none',
     color: 'inherit',
   };
-  const PF = "http://localhost:8000/Images/"
+  const PF = "https://blog-app-krzv.onrender.com/Images/"
   return (
     <div className='Single-Post'>
       <Topbar />
